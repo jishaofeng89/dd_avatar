d83dcc9d3382c5256d739f1a19bf4a8678cd95f9
jishaofeng89 <1195957057@qq.com>
2020-02-22 10:09:57 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:dd_avatar/app.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+// Sets a platform override for desktop to avoid exceptions. See
+// https://flutter.dev/desktop#target-platform-override for more info.
+void _enablePlatformOverrideForDesktop() {
+  if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+}
+
+void main() {
+  _enablePlatformOverrideForDesktop();
+  runApp(MyApp());
+}
